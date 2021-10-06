ad434dfeca4a9d4480e2ed420da00f097f112c1b
Paolo Lami <paololami2000@gmail.com>
2021-10-06 20:48:50 +0000

    test

--- Joystick.py
+++ Joystick.py
@@ -1,31 +1,10 @@
 import smbus
+import PCF8591
 
-class PCF8591:
-
-  def __init__(self,address):
-    self.bus = smbus.SMBus(1)
-    self.address = address
-
-  def read(self,chn): #channel
-      try:
-          self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
-          self.bus.read_byte(self.address) # dummy read to start conversion
-      except Exception as e:
-          print ("Address: %s \n%s" % (self.address,e))
-      return self.bus.read_byte(self.address)
-
-  def write(self,val):
-      try:
-          self.bus.write_byte_data(self.address, 0x40, int(val))
-      except Exception as e:
-          print ("Error: Device address: 0x%2X \n%s" % (self.address,e))
-          
 class Joystick:
   
-  def __init__(self,address,xlocation,ylocation):
+  def __init__(self,address):
     self.ADC = PCF8591(address)
-    self.xlocation=xlocation
-    self.ylocation=ylocation
     
   def getX(self):
     self.xlocation=self.ADC.read(1)
@@ -37,8 +16,9 @@ class Joystick:
 
 try:
   while 1:
-    coordinates= Joystick("test","127","127")
-    print(str(coordinates.xlocation)+" , ")
-    print(str(coordinates.ylocation) + "\n")
+    x = Joystick.getX("127")
+    y = Joystick.getY("127")
+    print(str(x)+" , ")
+    print(str(y) + "\n")
 except KeyboardInterrupt:
   print("Exiting...")
