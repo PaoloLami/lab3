de2711e44beca4a26d8eea7704bd2d029466ef65
Paolo Lami <paololami2000@gmail.com>
2021-10-06 20:24:53 +0000

    test

--- Joystick.py
+++ Joystick.py
@@ -6,17 +6,17 @@ class Joystick:
     self.ADC = PCF8591(bus,address)
     
   def getX(self):
-    self.xlocation=ADC.read(1)
+    self.xlocation=ADC.write(ADC.read(1))
     return(self.xlocation)
   
   def getY(self):
-    self.ylocation=ADC.read(2)
+    self.ylocation=ADC.write(ADC.read(2))
     return(self.ylocation)
 
 try:
   while 1:
-    x = Joystick.getX()
-    y = Joystick.getY() 
+    x = Joystick.getX(x)
+    y = Joystick.getY(y) 
     print(str(x)+" , ")
     print(str(y) + "\n")
 except KeyboardInterrupt:
